7833fbbcab2436770962cbdf3252056b29e020a0
Gastón <gastongithub@gmail.com>
2022-04-20 11:20:20 -0300

    IEnumerable error null

---
+++ UraniaWeb/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace UraniaWeb.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Article> ListaDeArticulos { get; set; }
+    }
+}
